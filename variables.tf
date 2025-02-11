06deafa0239828b98bf6e8ff88caf2e84e973875
Subrahmanya Gonella <164250924+satishgonella2024@users.noreply.github.com>
2025-02-11 14:19:54 +0000

    Initial commit with basic VPC infrastructure

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
